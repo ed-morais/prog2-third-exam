2037b4a68697740ac8380dc77b19e171b5ce3f9d
Edilson Morais <moraisbritoedilson@gmail.com>
2022-08-15 13:54:47 -0300

    initial commit

---
+++ bin/prog2_third_exam.dart
@@ -0,0 +1,5 @@
+import 'package:prog2_third_exam/prog2_third_exam.dart' as prog2_third_exam;
+
+void main(List<String> arguments) {
+  print('Hello world: ${prog2_third_exam.calculate()}!');
+}
---
+++ test/prog2_third_exam_test.dart
@@ -0,0 +1,8 @@
+import 'package:prog2_third_exam/prog2_third_exam.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
