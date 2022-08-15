d9ff7a38c82c4d91ae5b19e4b3afc4510e3505f2
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-08-15 16:59:04 -0300

    feat: add getter method that returns a list of elements

--- lib/elements.dart
+++ lib/elements.dart
@@ -1,1 +1,35 @@
-class Elements {}
+import 'package:prog2_third_exam/element.dart';
+import 'dart:io';
+
+class Elements {
+  late List<String> _elements;
+  List<Element> iterable = [];
+
+  Elements() {
+    _elements = File('data/elements.csv').readAsLinesSync();
+  }
+
+  List<Element> get elements {
+     for (var c = 1; c < _elements.length; c++) {
+      final data = _elements[c].split(',');
+      final elem = Element(
+        int.parse(data[0]),
+        data[1],
+        data[2],
+        data[3],
+      );
+      iterable.add(elem);
+    }
+    return iterable;
+  }
+}
+
+void main() {
+  Elements elements = Elements();
+
+  elements.elements.forEach(
+    (elem) {
+      print(elem);
+    },
+  );
+}
