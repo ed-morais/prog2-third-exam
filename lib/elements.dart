8304d7f022f25d74c8ca8facfc5f80241f6852ad
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-08-15 17:51:53 -0300

    feat: make element class iterable

--- lib/elements.dart
+++ lib/elements.dart
@@ -1,27 +1,40 @@
 import 'package:prog2_third_exam/element.dart';
 import 'dart:io';
 
-class Elements {
-  late List<String> _elements;
-  List<Element> iterable = [];
+class Elements extends Iterable {
+  final List<Element> _elements = [];
 
   Elements() {
-    _elements = File('data/elements.csv').readAsLinesSync();
-  }
-
-  List<Element> get elements {
-     for (var c = 1; c < _elements.length; c++) {
-      final data = _elements[c].split(',');
+    List<String> file = File('data/elements.csv').readAsLinesSync();
+    
+    for (var c = 1; c < file.length; c++) {
+      final data = file[c].split(',');
       final elem = Element(
         int.parse(data[0]),
         data[1],
         data[2],
         data[3],
       );
-      iterable.add(elem);
+      _elements.add(elem);
     }
-    return iterable;
   }
+
+  // List<Element> get elements {
+  //    for (var c = 1; c < _elements.length; c++) {
+  //     final data = _elements[c].split(',');
+  //     final elem = Element(
+  //       int.parse(data[0]),
+  //       data[1],
+  //       data[2],
+  //       data[3],
+  //     );
+  //     iterable.add(elem);
+  //   }
+  //   return iterable;
+  // }
+
+  @override
+  Iterator<Element> get iterator => _elements.iterator;
 }
 
 void printTabbed(
@@ -49,7 +62,7 @@ void main() {
   Elements elements = Elements();
 
   printTabbed('SYMBOL\tNAME\tLATIN\tWEIGHT', tabs);
-  elements.elements.forEach(
+  elements.forEach(
     (elem) {
       // ! no começo da string 'text' é necessário usar apenas 'elem' ao invés de 'elem.symbol'
       final text = '${elem.symbol}\t${elem.name}\t${elem.latinName}\t${elem.weight}';
