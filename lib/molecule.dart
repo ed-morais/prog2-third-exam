72ac9ca620097565a92b7dec722aac313143b00c
Edilson Morais <moraisbritoedilson@gmail.com>
2022-08-16 13:50:24 -0300

    adding molecule constructor, regex and getter

--- lib/molecule.dart
+++ lib/molecule.dart
@@ -1,1 +1,21 @@
-class Molecule {}
+class Molecule implements Comparable {
+  final String moleculeFormula;
+
+  Molecule({required this.moleculeFormula}) {
+    var regex = RegExp(r'([A-Z][a-z]*)(\d*)');
+    if (formula.isEmpty) {
+      throw ArgumentError('Formula cannot be empty!');
+    }
+  }
+
+  String get formula {
+    return moleculeFormula;
+  }
+
+  @override
+  int compareTo(other) {
+    // TODO: implement compareTo
+    throw UnimplementedError();
+  }
+  // regex: [A-Z][a-z]?\d*
+}
